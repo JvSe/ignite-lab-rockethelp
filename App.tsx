91c93535117489c77b27d889c9d7df9be0af64a1
João Vitor Soares <joaovitorsoares12@gmail.com>
2022-07-21 13:36:59 -0300

    Feat: Aula 2

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
+import { Home } from './src/screens/Home';
+import { Register } from './src/screens/Register';
 import { Loading } from './src/components/Loading';
+
+import { Routes } from './src/routes';
 
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -18,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/screens/Details.tsx
@@ -0,0 +1,18 @@
+import { useRoute } from '@react-navigation/native';
+import { VStack, Text } from 'native-base';
+import { Header } from '../components/Header';
+
+type DetailsRouteParams = {
+  orderId: string;
+}
+
+export function Details() {
+  const route = useRoute();
+  const { orderId } = route.params as DetailsRouteParams;
+  return (
+    <VStack flex={1} bg="gray.700">
+        <Header title='Solicitação'/>
+        <Text color="white">{orderId}</Text>
+    </VStack>
+  );
+}
